ffec3daf6cecd4d806bf7d3884751d4cb131cfff
satishkumarss <42444812+satishkumarss@users.noreply.github.com>
2018-08-30 00:53:27 +0530

    Update Occurence.java

--- Occurenceofcharacters/src/stringpack/Occurence.java
+++ Occurenceofcharacters/src/stringpack/Occurence.java
@@ -9,16 +9,13 @@ public class Occurence {
 		int d=0;
 		int lofreq=0;
 		int hifreq=0;
-		
 		for(int i=0;i<l;i++)
 		{
 			count=0;
 			char  c=s.charAt(0);
 			for(int j=0;j<l;j++) {
 			if(c==s.charAt(j))
-			{
-				count++;
-			}
+			     count++;
 			}
 			s=s.replace(c+"","");
 			d=l-s.length();
