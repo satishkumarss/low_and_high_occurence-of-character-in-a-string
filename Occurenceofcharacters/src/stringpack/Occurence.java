e82275a3acaca8ad2ed7686abcd635385fda81b9
satya <satishkumarsama@gmail.com>
2018-08-30 00:51:38 +0530

    commited -at finding low and high occurence of characters in a string

--- Occurenceofcharacters/src/stringpack/Occurence.java
+++ Occurenceofcharacters/src/stringpack/Occurence.java
@@ -3,8 +3,34 @@ package stringpack;
 public class Occurence {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String s="aabbccddeeeffffgg";
+		int l=s.length();
+		int count=0;
+		int d=0;
+		int lofreq=0;
+		int hifreq=0;
+		
+		for(int i=0;i<l;i++)
+		{
+			count=0;
+			char  c=s.charAt(0);
+			for(int j=0;j<l;j++) {
+			if(c==s.charAt(j))
+			{
+				count++;
+			}
+			}
+			s=s.replace(c+"","");
+			d=l-s.length();
+			l=l-d;
+			if(count>hifreq)
+				hifreq=count;
+			else
+				lofreq=count;
+		}
 
+		System.out.println(lofreq);
+		System.out.println(hifreq);
 	}
 
 }
